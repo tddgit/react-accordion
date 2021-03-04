c34a1d269183cb6db2494b71498078040f80164f
Stas <holidayonmail@gmail.com>
2021-03-04 15:26:57 +0300

    Eslint settled

---
+++ _GITHUB/webpack-starters-typescript/webpack.config.js
@@ -0,0 +1,33 @@
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    // mode defaults to 'production' if not set
+    mode,
+
+    entry: './src/index.ts',
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+        ],
+    },
+
+    // allows importing '.ts' files without the extension
+    // e.g. `import example from './example'`
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+
+    devtool: 'source-map',
+
+    // required if using webpack-dev-server
+    devServer: {
+        contentBase: './dist',
+    },
+};
