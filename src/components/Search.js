7a1bd1ab298fe40f32c7bd241834c67942b8daec
Stas <holidayonmail@gmail.com>
2021-03-16 22:28:44 +0300

    Added Dropdown finished Search components

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,100 @@
  * @file Searching component for Wikipedia.
  */
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
-    console.log('I RUN WITH EVERY RENDER');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
     useEffect(() => {
-        console.log('I RUN ON EVERY RERENDER AND INITIAL RENDER');
+        console.log('Before setting timeout');
+        const timerId = setTimeout(() => {
+            console.log('Timeout setDebouncedTerm');
+            setDebouncedTerm(term);
+        }, 1000);
+        return () => {
+            console.log('Clear timeout');
+            clearTimeout(timerId);
+        };
     }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(
+                `https://en.wikipedia.org/w/api.php`,
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: debouncedTerm,
+                    },
+                },
+            );
+            console.log('setResults');
+            setResults(data.query.search);
+        };
+        search();
+    }, [debouncedTerm]);
+
+    // eslint-disable-next-line consistent-return
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const { data } = await axios.get(
+    //             `https://en.wikipedia.org/w/api.php`,
+    //             {
+    //                 params: {
+    //                     action: 'query',
+    //                     list: 'search',
+    //                     origin: '*',
+    //                     format: 'json',
+    //                     srsearch: term,
+    //                 },
+    //             },
+    //         );
+    //
+    //         setResults(data.query.search);
+    //     };
+    //
+    //     if (term && !results.length) {
+    //         search();
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //                 search();
+    //             }
+    //         }, 1000);
+    //
+    //         return () => {
+    //             clearTimeout(timeoutId);
+    //         };
+    //     }
+    // }, [term, results.length]);
+
+    // eslint-disable-next-line lodash/prefer-lodash-method
+    const renderedResults = results.map((result) => {
+        return (
+            <div className={'item'} key={result.pageid}>
+                <div className="right floated content"></div>
+                <a
+                    href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    className={'ui button'}
+                >
+                    Go
+                </a>
+                <div className={'content'}>
+                    <div className={'header'}>{result.title}</div>
+                    <span
+                        dangerouslySetInnerHTML={{ __html: result.snippet }}
+                    ></span>
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <div className="ui form"></div>
@@ -22,6 +109,7 @@ const Search = () => {
                     onChange={(e) => setTerm(e.target.value)}
                 />
             </div>
+            <div className={'ui celled list'}>{renderedResults}</div>
         </div>
     );
 };
